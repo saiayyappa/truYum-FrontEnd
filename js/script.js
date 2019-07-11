22afe51c428d8dea963d2400c8eb130d83afddbf
saiayyappaor <saiayyappaor@gmail.com>
2019-07-11 17:55:40 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Include truYum form validation functions here
+var adminData = [
+    { name: 'Sandwich', price: 99, active: 'Yes', dateOfLaunch: '15/03/2017', category: 'Main Course', freeDelivery: 'Yes' },
+    { name: 'Burger', price: 129, active: 'Yes', dateOfLaunch: '23/12/2017', category: 'Main Course', freeDelivery: 'No' },
+    { name: 'Pizza', price: 149, active: 'Yes', dateOfLaunch: '21/08/2017', category: 'Main Course', freeDelivery: 'No' },
+    { name: 'French Fries', price: 57, active: 'No', dateOfLaunch: '02/07/2017', category: 'Starter', freeDelivery: 'Yes' },
+    { name: 'Chocolate Brownies', price: 32, active: 'Yes', dateOfLaunch: '02/11/2022', category: 'Dessert', freeDelivery: 'Yes' }
+]
+var customerData = [
+    { name: 'Sandwich', freeDelivery: 'Yes', price: 99, category: 'Main Course' },
+    { name: 'Burger', freeDelivery: 'No', price: 129, category: 'Main Course' },
+    { name: 'Pizza', freeDelivery: 'No', price: 149, category: 'Main Course' }
+]
