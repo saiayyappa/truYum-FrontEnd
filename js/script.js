7a0856a7b7ac2de02384fe9caba617f053d5c5a9
saiayyappaor <saiayyappaor@gmail.com>
2019-07-12 18:00:01 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,13 +1,104 @@
 // Include truYum form validation functions here
-var adminData = [
+var foodDatas = [
     { name: 'Sandwich', price: 99, active: 'Yes', dateOfLaunch: '15/03/2017', category: 'Main Course', freeDelivery: 'Yes' },
     { name: 'Burger', price: 129, active: 'Yes', dateOfLaunch: '23/12/2017', category: 'Main Course', freeDelivery: 'No' },
     { name: 'Pizza', price: 149, active: 'Yes', dateOfLaunch: '21/08/2017', category: 'Main Course', freeDelivery: 'No' },
     { name: 'French Fries', price: 57, active: 'No', dateOfLaunch: '02/07/2017', category: 'Starter', freeDelivery: 'Yes' },
     { name: 'Chocolate Brownies', price: 32, active: 'Yes', dateOfLaunch: '02/11/2022', category: 'Dessert', freeDelivery: 'Yes' }
 ]
-var customerData = [
-    { name: 'Sandwich', freeDelivery: 'Yes', price: 99, category: 'Main Course' },
-    { name: 'Burger', freeDelivery: 'No', price: 129, category: 'Main Course' },
-    { name: 'Pizza', freeDelivery: 'No', price: 149, category: 'Main Course' }
-]
+
+function admin() {
+    let adminContent = document.getElementById('admin-content');
+    let customerContent = document.getElementById('customer-content');
+
+    adminContent.style.display = 'block';
+    customerContent.style.display = 'none';
+}
+
+function customer() {
+    let adminContent = document.getElementById('admin-content');
+    let customerContent = document.getElementById('customer-content');
+
+    adminContent.style.display = 'none';
+    customerContent.style.display = 'block';
+}
+
+const displayDataCustomer = function (foodDatas) {
+    let tBody = document.getElementById('tBodyCustomer');
+    for (let foodData of foodDatas) {
+
+        let row = document.createElement('tr');
+        let col1 = document.createElement('td');
+        let col2 = document.createElement('td');
+        let col3 = document.createElement('td');
+        let col4 = document.createElement('td');
+        let col5 = document.createElement('td');
+        let addToCart = document.createElement('a');
+
+        col1.textContent = foodData.name;
+        row.appendChild(col1);
+
+        col2.textContent = foodData.freeDelivery;
+        row.appendChild(col2);
+
+        col3.textContent = foodData.price;
+        row.appendChild(col3);
+
+        col4.textContent = foodData.category;
+        row.appendChild(col4);
+
+        addToCart.href = "#";
+        addToCart.textContent = "Add to Cart";
+        // addToCart.classList.add('mdl-button','mdl-js-button');
+
+        col5.appendChild(addToCart);
+        row.appendChild(col5);
+        tBody.appendChild(row);
+
+    }
+}
+
+const displayDataAdmin = function (foodDatas) {
+    let tBody = document.getElementById('tBodyAdmin');
+    for (let foodData of foodDatas) {
+
+        let row = document.createElement('tr');
+        let col1 = document.createElement('td');
+        let col2 = document.createElement('td');
+        let col3 = document.createElement('td');
+        let col4 = document.createElement('td');
+        let col5 = document.createElement('td');
+        let col6 = document.createElement('td');
+        let col7 = document.createElement('td');
+        let edit = document.createElement('a');
+
+        col1.textContent = foodData.name;
+        row.appendChild(col1);
+
+        col2.textContent = foodData.price;
+        row.appendChild(col2);
+
+        col3.textContent = foodData.active;
+        row.appendChild(col3);
+
+        col4.textContent = foodData.dateOfLaunch;
+        row.appendChild(col4);
+
+        col5.textContent = foodData.category;
+        row.appendChild(col5);
+
+        col6.textContent = foodData.freeDelivery;
+        row.appendChild(col6);
+
+        edit.href = "#";
+        edit.textContent = "Edit";
+        // edit.classList.add('mdl-button','mdl-js-button');
+
+        col7.appendChild(edit);
+        row.appendChild(col7);
+        tBody.appendChild(row);
+    }
+}
+
+displayDataCustomer(foodDatas);
+displayDataAdmin(foodDatas);
